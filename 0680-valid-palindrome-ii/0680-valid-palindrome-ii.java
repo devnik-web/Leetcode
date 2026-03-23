5a3164c7a8036612b78e665e537ddab695dbecce
Nikhil Sharma <nikhil.sharma.studymail@gmail.com>
2026-03-23 22:14:26 +0530

    Time: 5 ms (16.16%), Space: 47.5 MB (95.59%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,42 +1,31 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
+        int n = s.length();
 
-        while (left<right) {
-            if (s.charAt(left)==s.charAt(right)) {
-                left++;
-                right--;
-            } else {
-                String sLeft=s.substring(0, left) + s.substring(left+1);
-                String sRight=s.substring(0, right) + s.substring(right+1);
+        int i = 0, j = n-1;
 
-                int l=0,r=sLeft.length()-1;
-                boolean leftValid=true;
-                while (l<r) {
-                    if(sLeft.charAt(l)!=sLeft.charAt(r)){
-                        leftValid = false;
-                        break;
-                    }
-                    l++;
-                    r--;
-                }
-
-                l=0; 
-                r=sRight.length()-1;
-                boolean rightValid=true;
-                while(l<r) {
-                    if(sRight.charAt(l)!=sRight.charAt(r)){
-                        rightValid=false;
-                        break;
-                    }
-                    l++;
-                    r--;
-                }
-
-                return leftValid || rightValid;
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i+1,j) || isPalindrome(s, i, j-1);
             }
+
+            i++;
+            j--;
         }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int i, int j) {
+        
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
         return true;
     }
 }
