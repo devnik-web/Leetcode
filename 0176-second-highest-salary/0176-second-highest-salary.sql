50a494172444731db016e2089a7dcad86ec949b1
Nikhil Sharma <nikhil.sharma.studymail@gmail.com>
2026-03-24 22:35:26 +0530

    Time: 282 ms (31.15%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,2 +1,3 @@
-# Write your MySQL query statement below
-select coalesce((select distinct salary from employee order by salary desc limit 1 offset 1),NULL) as SecondHighestSalary;
+select max(salary) as SecondHighestSalary
+from employee
+where salary < (select max(salary) from employee);
